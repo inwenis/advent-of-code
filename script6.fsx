06b00b246039c6a92dde7975e2e738df7f0e53b3
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2022-02-15 15:56:34 +0100

    ex6

---
+++ script6.fsx
@@ -0,0 +1,17 @@
+let mutable fish =
+    // "3,4,3,1,2".Split(",")
+    System.IO.File.ReadAllText("input61.txt").Split(",")
+    |> List.ofArray
+    |> List.map int
+
+let day (fish:int list) = [
+    for f in fish do
+        match f with
+        | 0 -> yield! [6;8]
+        | _ -> yield f - 1
+    ]
+
+for _ in [1..80] do
+    fish <- day fish
+
+printfn  "%A" fish.Length
