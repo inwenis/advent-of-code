76d6a6d09bcfffbf2d725fb7f2e985999ca04140
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2022-02-20 10:31:10 +0100

    solve ex6.2 with constant space

---
+++ c/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+Console.WriteLine("hello");
+
+var fish =
+    File.ReadAllText("../input61.txt")
+    .Split(",")
+    .Select(int.Parse)
+    .ToList();
+
+void day(List<int> fish)
+{
+    var fc = fish.Count;
+    for (int j = 0; j < fc; j++)
+    {
+        if (fish[j] == 0) {
+            fish[j] = 6;
+            fish.Add(8);
+        } else {
+            fish[j]--;
+        }
+    }
+}
+
+void day2(List<byte> fish)
+{
+    var fc = fish.Count;
+    for (int j = 0; j < fc; j++)
+    {
+        if (fish[j] == 0) {
+            fish[j] = 6;
+            fish.Add(8);
+        } else {
+            fish[j]--;
+        }
+    }
+}
+
+// for (int i = 0; i < 80; i++)
+//     day(fish);
+
+// System.Console.WriteLine(fish.Count);
+
+// var fish2 =
+//     File.ReadAllText("../input61.txt")
+//     .Split(",")
+//     .Select(int.Parse)
+//     .ToList();
+
+// var d = 80;
+
+// var sum = 0;
+// for (int i = 0; i < 1; i++)
+// {
+//     var s = fish2[i];
+//     System.Console.WriteLine((6 - fish2[i] + d ) / 7);
+//     sum += (6 - fish2[i] + d ) / 7;
+//     int j = 0;
+//     int to_add;
+//     do
+//     {
+//         to_add = (d - 2 - s - 7 * j)/7;
+//         System.Console.WriteLine(to_add);
+//         if (to_add > 0) sum += to_add;
+//         j++;
+//     } while(to_add > 0);
+// }
+
+// // var total =
+// //     fish2
+// //     .Select(x => Math.Pow(2, (6-x+d)/7 ))
+// //     .Sum();
+
+// System.Console.WriteLine(sum);
+
+var fish2 =
+    File.ReadAllText("../input61.txt")
+    .Split(",")
+    .Select(byte.Parse)
+    .ToList();
+
+var sum = 0;
+var z = 0;
+foreach (var item in fish2)
+{
+    var singleflist = new List<byte>(15000000);
+    singleflist.Add(item);
+    for (int i = 0; i < 256; i++) {
+        day2(singleflist);
+        if (i%10==0) System.Console.WriteLine($"day={i}");
+    }
+    sum += singleflist.Count;
+    System.Console.WriteLine($"{z++} / {fish2.Count}");
+}
+
+System.Console.WriteLine(sum);
+
--- script6.fsx
+++ script6.fsx
@@ -1,20 +1,47 @@
-let mutable fish =
+let day (fish:Map<int, int64>) =
+    fish
+    |> Seq.toList
+    |> List.map (fun kvp -> kvp.Key, kvp.Value)
+    |> List.map (fun (k, c) ->
+        match k with
+        | 0 -> [6, c; 8,c]
+        | x -> [x-1, c]
+        )
+    |> List.concat
+    |> List.groupBy (fun (k, c) -> k)
+    |> List.map     (fun (k, g) -> k, g |> List.map snd |> List.sum)
+    |> Map.ofList
+
+let fishi =
     // "3,4,3,1,2".Split(",")
     System.IO.File.ReadAllText("input61.txt").Split(",")
     |> List.ofArray
     |> List.map int
+    |> List.groupBy id
+    |> List.map (fun (k, l) -> k, l.Length |> int64)
+    |> Map.ofList
 
-let day (fish:int list) = [
-    for f in fish do
-        match f with
-        | 0 -> yield! [6;8]
-        | _ -> yield f - 1
-    ]
+let getOrDefault (m:Map<int,int64>) (k:int) (d:int64) =
+    match m.TryFind k with
+    | Some v -> v
+    | None   -> d
+
+let mutable fish =
+    [0..8]
+    |> List.map (fun x -> x, getOrDefault fishi x 0)
+    |> Map.ofList
 
 for _ in [1..80] do
     fish <- day fish
 
-printfn  "%A" fish.Length
+printfn  "%A" (Map.values fish |> Seq.sum )
+
+let mutable fish2 =
+    [0..8]
+    |> List.map (fun x -> x, getOrDefault fishi x 0)
+    |> Map.ofList
 
 for _ in [1..256] do
-    fish <- day fish
+    fish2 <- day fish2
+
+printfn  "%A" (Map.values fish2 |> Seq.sum )
